9898eefd760a567ad66836ceb60c48301ff74283
JongoDB <Jonathanrannabargar@gmail.com>
2026-01-17 19:26:17 -0500

    feat(wizard): add steps index

---
+++ frontend/src/components/wizard/steps/index.ts
@@ -0,0 +1,6 @@
+// frontend/src/components/wizard/steps/index.ts
+export { ScenarioSelection } from './ScenarioSelection';
+export { ZoneConfiguration } from './ZoneConfiguration';
+export { SystemSelection } from './SystemSelection';
+export { ConfigurationOptions } from './ConfigurationOptions';
+export { ReviewAndDeploy } from './ReviewAndDeploy';
